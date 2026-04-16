d9292f39ddcbca8e09375ff453bb5d250204e853
Haymooed <167770628+Haymooed@users.noreply.github.com>
2026-04-16 11:32:51 +0930

    Update __init__.py

--- community_challenge/community_challenge/__init__.py
+++ community_challenge/community_challenge/__init__.py
@@ -1,8 +1,10 @@
 from typing import TYPE_CHECKING
 from .cog import ChallengesCog
+from .. import patch
 
 if TYPE_CHECKING:
     from ballsdex.core.bot import BallsDexBot
 
 async def setup(bot: "BallsDexBot"):
+    patch.apply_patches()
     await bot.add_cog(ChallengesCog(bot))
