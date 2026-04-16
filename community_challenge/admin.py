a792c8e26b7456efe56e658bd416dfc60e4f9345
Haymooed <167770628+Haymooed@users.noreply.github.com>
2026-04-16 12:06:55 +0930

    Update admin.py

--- community_challenge/admin.py
+++ community_challenge/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from .models import Challenge, ChallengeParticipant
+from .models import Challenge, ChallengeParticipant, ChallengeReward
+
+class ChallengeRewardInline(admin.TabularInline):
+    model = ChallengeReward
+    extra = 1
+    autocomplete_fields = ["ball"] # Gives you a nice search bar to find the ball
 
 @admin.register(Challenge)
 class ChallengeAdmin(admin.ModelAdmin):
     list_display = ('name', 'start_time', 'end_time', 'goal_type', 'active')
     list_filter = ('active', 'goal_type')
+    inlines = [ChallengeRewardInline] # Puts the rewards UI inside the challenge page
 
 @admin.register(ChallengeParticipant)
 class ChallengeParticipantAdmin(admin.ModelAdmin):
